1433ecf77ffd4fbce2ee541e0f0f0acb40590229
songyumeng <songyumeng@supermap.com>
2019-05-16 16:07:49 +0800

    【update】fix 调试失败的问题   review by liqian

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,8 +36,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      // 'vue$': 'vue/dist/vue.esm.js',
-      // '@': resolve('src'),
+      'vue$': 'vue/dist/vue.esm.js'
       // '@libs': resolve('libs')
     }
   },
