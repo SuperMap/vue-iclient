f5d201fb0b1e06f6eeb008e25da8658ef9785c3d
luoxiao <luoxiao@supermap.com>
2019-05-31 10:50:51 +0800

    【fix】修复lang里const变量提升问题 review by songym

--- src/common/_lang/index.js
+++ src/common/_lang/index.js
@@ -4,19 +4,6 @@ import Cookies from 'js-cookie';
 import enLocale from './en';
 import zhLocale from './zh';
 import clonedeep from 'lodash.clonedeep';
-
-const dateTimeFormats = {
-  en: enLocale.dateTimeFormat,
-  zh: zhLocale.dateTimeFormat
-};
-
-if (!Vue.prototype.hasOwnProperty('$i18n')) {
-  Object.defineProperty(Vue.prototype, '$i18n', {
-    get: function get() {
-      return i18n;
-    }
-  });
-}
 Vue.use(VueI18n);
 
 const messages = {
@@ -27,13 +14,23 @@ const messages = {
     ...zhLocale
   }
 };
+const dateTimeFormats = {
+  en: enLocale.dateTimeFormat,
+  zh: zhLocale.dateTimeFormat
+};
 const i18n = new VueI18n({
   dateTimeFormats,
   locale: getLanguage(),
   fallbackLocale: 'zh',
   messages
 });
-
+if (!Vue.prototype.hasOwnProperty('$i18n')) {
+  Object.defineProperty(Vue.prototype, '$i18n', {
+    get: function get() {
+      return i18n;
+    }
+  });
+}
 export function setLocale(locales) {
   i18n.mergeLocaleMessage(i18n.locale, locales);
 }
