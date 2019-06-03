42207424e3acc2d7fb2cc69d8a22fca334b088e1
liqian1 <liqian1@supermap.com>
2019-06-03 14:57:30 +0800

    【fix】 fix mbgl 声明错误问题 review by songym

--- src/sfc.d.ts
+++ src/sfc.d.ts
@@ -5,7 +5,7 @@
  * 而在代码中导入 *.vue 文件的时候，需要写上 .vue 后缀。
  * 原因还是因为 TypeScript 默认只识别 *.ts 文件，不识别 *.vue 文件
  */
-///<reference path="../node_modules/@types/mapbox-gl/index.d.ts"/>
+/// <reference types="mapbox-gl"/>
 
 // ignore warning---mapboxgl 建议模块引入声明 TODO
 declare var mapboxgl;
