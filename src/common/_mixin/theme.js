f4ad2b4204bdff4738899f86dea09f01a3bacdc1
songyumeng <songyumeng@supermap.com>
2019-06-21 18:37:20 +0800

    【fix】优化主题逻辑 review by xiongjj

--- src/common/_mixin/theme.js
+++ src/common/_mixin/theme.js
@@ -3,18 +3,13 @@ import globalEvent from '../_utils/global-event';
 export default {
   props: {
     background: {
-      type: String,
-      default: 'rgba(255,255,255,0.6)'
+      type: String
     },
     textColor: {
       type: String,
-      default: '#333'
     },
     colorGroup: {
-      type: Array,
-      default() {
-        return ['#3fb1e3', '#6be6c1', '#626c91', '#a0a7e6', '#c4ebad', '#96dee8'];
-      }
+      type: Array
     }
   },
   watch: {
@@ -25,22 +20,15 @@ export default {
       this.textColorsData = newValue;
     },
     colorGroup: function(newValue, oldValue) {
-      /**
-       * 以下操作是暂时解决方另一个问题引起的新问题
-       * 起因是data里面在给colorGroupsData赋值时优先取的theme里面的值，
-       * 如果theme里有值的话，就取不到props里colorGroup的值(也是json配置文件里面的初始化会失效)
-       * 但是AB项目在手动把内联样式转化为rem时，为了不影响store里面的propDatas,在转化rem之前总是先深拷贝一份propDatas的值
-       * 所以如果propDatas有里面Object或者Array类型的数据，其引用地址总会改变，即使其里面的值相同
-       */
-      this.colorGroupsData = JSON.stringify(newValue) !== JSON.stringify(oldValue) ? newValue : this.colorGroupsData;
+      this.colorGroupsData = newValue;
     }
   },
   data() {
     let theme = globalEvent.theme;
     return {
-      backgroundData: (theme && theme.background) || this.background,
-      textColorsData: (theme && theme.textColor) || this.textColor,
-      colorGroupsData: (theme && theme.colorGroup) || this.colorGroup
+      backgroundData: this.background || (theme && theme.background) ,
+      textColorsData: this.textColor || (theme && theme.textColor),
+      colorGroupsData: this.colorGroup || (theme && theme.colorGroup)
     };
   },
   mounted() {
@@ -49,7 +37,6 @@ export default {
       this.textColorsData = themeStyle.textColor;
       this.colorGroupsData = themeStyle.colorGroup;
       this.$emit('themeStyleChanged');
-      // this.themeStyle = Object.assign({}, this.themeStyle, themeStyle)
     });
   },
   computed: {
