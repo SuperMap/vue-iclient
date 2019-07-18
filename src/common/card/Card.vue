8a08664ea27803fd3cd2a19361c212df1772e526
xiongjiaojiao <xiongjiaojiao@supermap.com>
2019-07-18 16:10:55 +0800

    update card内容显示优化, review  by songym

--- src/common/card/Card.vue
+++ src/common/card/Card.vue
@@ -113,6 +113,9 @@ export default {
       if (newVal && !oldVal) {
         this.isShow = this.collapsed;
         this.toggleTransition(this.collapsed ? 'enter' : 'leave');
+      } else if(!newVal) {
+        // 如果iconClass 为空 则默认显示内容
+        this.isShow = true;
       }
     }
   },
