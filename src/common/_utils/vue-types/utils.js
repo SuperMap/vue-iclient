024b86f84a88ab045e5ddceb21ec4bb3c5bfa8ff
songyumeng <songyumeng@supermap.com>
2021-05-04 00:30:20 +0800

    Update utils.js

--- src/common/_utils/vue-types/utils.js
+++ src/common/_utils/vue-types/utils.js
@@ -48,7 +48,7 @@ export const isInteger =
 export const isArray =
   Array.isArray ||
   function(value) {
-    return Object.prototype.toSrtring.call(value) === '[object Array]';
+    return Object.prototype.toString.call(value) === '[object Array]';
   };
 
 /**
@@ -57,7 +57,7 @@ export const isArray =
  * @param {any} value - Value to check
  * @returns {boolean}
  */
-export const isFunction = value => Object.prototype.toSrtring.call(value) === '[object Function]';
+export const isFunction = value => Object.prototype.toString.call(value) === '[object Function]';
 
 /**
  * Adds a `def` method to the object returning a new object with passed in argument as `default` property
