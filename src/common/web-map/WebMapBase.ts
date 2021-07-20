e9676ac40b2d59ba9da79ed76e82478da2ae6136
qiwei <qiwei@supermap.com>
2021-07-20 19:49:26 +0800

    【fix】 修复打开多边线shp 坐标不对 review by luox

--- src/common/web-map/WebMapBase.ts
+++ src/common/web-map/WebMapBase.ts
@@ -5,9 +5,11 @@ import 'vue-iclient/static/libs/geostats/geostats';
 import 'vue-iclient/static/libs/json-sql/jsonsql';
 import isNumber from 'lodash.isnumber';
 import canvg from 'canvg';
-import WebMapService from 'vue-iclient/src/common/_utils/WebMapService';
-import { getColorWithOpacity } from 'vue-iclient/src/common/_utils/util';
-import { getProjection, registerProjection } from 'vue-iclient/src/common/_utils/epsg-define';
+
+import WebMapService from '../_utils/WebMapService';
+import { getColorWithOpacity } from '../_utils/util';
+import { getProjection, registerProjection } from '../../common/_utils/epsg-define';
+import { coordEach } from '@turf/meta';
 
 // 迁徙图最大支持要素数量
 const MAX_MIGRATION_ANIMATION_COUNT = 1000;
@@ -714,29 +716,16 @@ export default abstract class WebMapBase extends Events {
   protected transformFeatures(features) {
     features &&
       features.forEach((feature, index) => {
-        const geometryType = feature.geometry && feature.geometry.type;
         let coordinates = feature.geometry && feature.geometry.coordinates;
         if (!coordinates || coordinates.length === 0) {
           return;
         }
-        if (geometryType === 'LineString') {
-          coordinates.forEach((coordinate, index) => {
-            coordinate = this._unproject(coordinate);
-            coordinates[index] = coordinate;
-          }, this);
-        } else if (geometryType === 'Point') {
-          coordinates = this._unproject(coordinates);
-          feature.geometry.coordinates = coordinates;
-        } else if (geometryType === 'MultiPolygon' || geometryType === 'Polygon') {
-          coordinates.forEach((coordinate, index) => {
-            const coords = geometryType === 'MultiPolygon' ? coordinate[0] : coordinate;
-            coords.forEach((latlng, i) => {
-              latlng = this._unproject(latlng);
-              coords[i] = latlng;
-            });
-            coordinates[index] = coordinate;
-          });
-        }
+        coordEach(feature, (coordinates) => {
+          // @ts-ignore
+          let transCoordinates = this._unproject(coordinates);
+          coordinates[0] = transCoordinates[0];
+          coordinates[1] = transCoordinates[1];
+        })
         features[index] = feature;
       });
 
