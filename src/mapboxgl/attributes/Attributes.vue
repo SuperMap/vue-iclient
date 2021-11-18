9cf38042d83bbd430b300824a79c1877aefd4eb2
yangmengyue <yangmengyue@supermap.com>
2021-11-18 15:50:16 +0800

    【 fix 】优化属性表下拉框选项的 icon 与文字的间距； review by xiongjj

--- src/mapboxgl/attributes/Attributes.vue
+++ src/mapboxgl/attributes/Attributes.vue
@@ -14,8 +14,8 @@
         <span v-if="statistics.showTotal || statistics.showSelect">）</span>
       </div>
       <div class="sm-component-attributes__menu">
-        <sm-dropdown v-if="toolbar.enabled" placement="bottomRight">
-          <div class="ant-dropdown-link"><sm-icon :icon-style="{ color: '#ccc' }" type="menu" /></div>
+        <sm-dropdown :getPopupContainer="triggerNode => triggerNode.parentNode" v-if="toolbar.enabled" placement="bottomRight">
+          <div class="sm-component-dropdown-link"><sm-icon :icon-style="{ color: '#ccc' }" type="menu" /></div>
           <sm-menu slot="overlay">
             <sm-menu-item>
               <div v-if="toolbar.showClearSelected" @click="clearSelectedRows">
