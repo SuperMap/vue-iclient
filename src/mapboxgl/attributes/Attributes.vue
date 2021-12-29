ba1ddcf7a28db7e650d223f316a1c93eb5932942
xiongjiaojiao <xiongjiaojiao@supermap.com>
2021-12-29 18:10:56 +0800

    【fix】图层颜色标签名不显示; 属性表子菜单列表显示不全; review by songym

--- src/mapboxgl/attributes/Attributes.vue
+++ src/mapboxgl/attributes/Attributes.vue
@@ -14,9 +14,9 @@
         <span v-if="statistics.showTotal || statistics.showSelect">）</span>
       </div>
       <div class="sm-component-attributes__menu">
-        <sm-dropdown :getPopupContainer="triggerNode => triggerNode.parentNode" v-if="toolbar.enabled" placement="bottomRight">
+        <sm-dropdown v-if="toolbar.enabled" placement="bottomRight">
           <div class="sm-component-dropdown-link"><sm-icon :icon-style="{ color: '#ccc' }" type="menu" /></div>
-          <sm-menu slot="overlay">
+          <sm-menu slot="overlay" class="sm-component-attribute_dropdown-menu">
             <sm-menu-item>
               <div v-if="toolbar.showClearSelected" @click="clearSelectedRows">
                 {{ this.$t('attributes.clearSelected') }}
