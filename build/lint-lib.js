08ef6494956baa8e1e20ef8a7e05086e1712b454
luoxiao-supermap <luoxiao@supermap.com>
2022-06-30 18:35:54 +0800

    [fix]publish lint-lib报错 review by qiw

--- build/lint-lib.js
+++ build/lint-lib.js
@@ -5,23 +5,20 @@ let argv = JSON.parse(process.env['npm_config_argv']);
 let origin = argv.original;
 const type = origin[2].substring(1);
 
-let filePath = `../lib/${type}`;
-try {
-  fs.statSync(path.resolve(__dirname, filePath));
-} catch (err) {
-  filePath = `../lib`;
-}
+const filePath = `../lib/${type}`;
 const compFiles = fs.readdirSync(path.resolve(__dirname, filePath));
 compFiles.forEach(compName => {
   if (['index.js', 'init.js', 'style.js'].includes(compName) || compName.startsWith('_')) return;
+  const stylePath = path.resolve(__dirname, filePath, `${compName}/style`);
+  const styleJsPath = path.resolve(__dirname, filePath, `${compName}/style/css.js`);
   try {
-    fs.statSync(path.resolve(__dirname, filePath) + `\\${compName}\\style`);
-    fs.readFile(path.resolve(__dirname, filePath) + `\\${compName}\\style\\css.js`, 'utf8', function (err) {
+    fs.statSync(stylePath);
+    fs.readFile(styleJsPath , 'utf8', function (err) {
       if (err) {
-        throw new Error(`按需引入的lib包：没有${type}/lib/${compName}/style/css.js文件`);
+        throw new Error(`按需引入的lib包：${styleJsPath}不存在`);
       }
     });
   } catch (err) {
-    throw new Error(`按需引入的lib包：${type}/lib/${compName}下没有style文件夹`);
+    throw new Error(`按需引入的lib包：${stylePath}不存在`);
   }
 });
