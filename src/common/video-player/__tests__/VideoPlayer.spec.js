6bf3b56710985088f761f73c770d7ea856818624
luoxiao <luoxiao@supermap.com>
2022-09-07 20:07:29 +0800

    【feature】视频组件开放实例 review by qiw

--- src/common/video-player/__tests__/VideoPlayer.spec.js
+++ src/common/video-player/__tests__/VideoPlayer.spec.js
@@ -3,36 +3,69 @@ import SmVideoPlayer from '../VideoPlayer.vue';
 const localVue = createLocalVue();
 import { message } from 'ant-design-vue';
 localVue.prototype.$message = message;
+import mockVideo from 'video.js';
 
 describe('VideoPlayer.vue', () => {
   let wrapper;
   let pauseStub;
   let loadStub;
   beforeEach(() => {
-    pauseStub = jest.spyOn(window.HTMLMediaElement.prototype, 'pause')
-    .mockImplementation(() => {});
-    loadStub = jest.spyOn(window.HTMLMediaElement.prototype, 'load')
-    .mockImplementation(() => {});
+    const videojs = (videoObj, videoOptions, cb) => {
+     const res = {
+        el_: {
+          firstChild: {
+            id: 'video_id'
+          }
+        },
+        volume() {},
+        muted() {},
+        on() {},
+        one() {},
+        play() {},
+        pause() {},
+        currentTime() {},
+        getTech() {},
+        mergeOptions() {},
+        registerTech() {}
+      }
+      cb.apply(res);
+      return res;
+    }
+    mockVideo.mockImplementation(videojs);
+    pauseStub = jest.spyOn(window.HTMLMediaElement.prototype, 'pause').mockImplementation(() => {});
+    loadStub = jest.spyOn(window.HTMLMediaElement.prototype, 'load').mockImplementation(() => {});
     wrapper = null;
   });
 
   afterEach(() => {
     if (wrapper) {
       wrapper.destroy();
-    };
+    }
     pauseStub.mockRestore();
     loadStub.mockRestore();
   });
 
-  it('render', () => {
+  it('render', async done => {
     wrapper = mount(SmVideoPlayer, {
       localVue,
       propsData: {
-        url: 'fakeurl.mp4'
+        // url: 'fakeurl.mp4',
+        options: {
+          muted: true,
+          loop: false,
+          popupToPlay: false,
+          autoplay: false,
+          controlBar: true
+        }
       },
       attachToDocument: 'body'
     });
     expect(wrapper.find('.sm-component-video-player').exists()).toBe(true);
+    wrapper.vm.$on('instance-loaded', player => {
+      expect(player).not.toBeUndefined();
+      done();
+    });
+    await wrapper.vm.$nextTick();
   });
 
   it('change props', async () => {
