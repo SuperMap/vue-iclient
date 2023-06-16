509a719b9a824f69bd0263acf1094b0d591b0e61
zhaochengjun <luoxiao@supermap.com>
2023-06-16 21:26:59 +0800

    恢复边框样式不起作用缺陷 review by luoxiao

--- src/mapboxgl/web-map/control/identify/__tests__/Identify.spec.js
+++ src/mapboxgl/web-map/control/identify/__tests__/Identify.spec.js
@@ -3,6 +3,8 @@ import SmIdentify from '../Identify.vue';
 import Identify from '../index';
 import SmWebMap from '../../../WebMap.vue';
 import mapLoaded from 'vue-iclient/test/unit/mapLoaded.js';
+import { LineStyle, CircleStyle, FillStyle } from '../../../../_types/index'
+
 
 describe('Identify.vue', () => {
   let wrapper;
@@ -102,6 +104,88 @@ describe('Identify.vue', () => {
       layers: ['民航数据']
     });
     expect(spy).toHaveBeenCalledTimes(1);
+    done();
+  });
+
+  it('clcik layer on map', async done => {
+    // 设置外界传入的参数，对面填充和边框的颜色
+    const color = '#FF0000'
+    wrapper = mount({
+      template: 
+      `
+        <sm-web-map style="height:700px" :mapId="mapInfo">
+          <sm-identify 
+            :fields="['地区','人口数']"
+            :layerStyle="layerStyles"
+            :layers="['第七次人口普查全国各省人口数(未包含港澳台','第七次人口普查全国各省人口数(未包含港澳台-strokeLine']">
+          </sm-identify>
+        </sm-web-map> `,
+      //  `<sm-web-map style="height:700px" :mapId="mapInfo">
+      //     <sm-identify :layers="['China']" :fields="['机场','同比增速%','2017旅客吞吐量（人次）']"></sm-identify>
+      //   </sm-web-map>`,
+      components: {
+        SmIdentify,
+        SmWebMap
+      },
+      data() {
+        return {
+          mapInfo: {
+            extent: {
+              leftBottom: { x: 0, y: 0 },
+              rightTop: { x: 0, y: 0 }
+            },
+            level: 5,
+            center: { x: 0, y: 0 },
+            baseLayer: {
+              mapId: 1160955209, 
+              layerType: 'TILE',
+              name: 'China',
+              url: 'http://172.16.14.44:8190/iportal'
+            },
+            layers: [],
+            description: '',
+            projection: 'EPSG:3857',
+            title: 'testMap',
+            version: '1.0'
+          },
+          layerStyles: {
+            line: new LineStyle({ 'line-width': 3, 'line-color': '#3fb1e3' }),
+            circle: new CircleStyle({ 'circle-color': '#3fb1e3', 'circle-radius': 6 }),
+            fill: new FillStyle({ 'fill-color': '#3fb1e3', 'fill-opacity': 0.8 }),
+            stokeLine: new LineStyle({ 'line-width': 3, 'line-color': color }),
+          }
+        };
+      }
+    });
+    const callback = jest.fn();
+    identifyWrapper = wrapper.vm.$children[0].$children[0];
+    identifyWrapper.$on('loaded', callback);
+    await mapLoaded(wrapper.vm.$children[0]);
+    identifyWrapper.setViewModel()
+    const spy = jest.spyOn(identifyWrapper.viewModel, 'addOverlayToMap');
+    // 手动设置点击事件的参数，确认是再点击面
+    const layer = {
+      "id":"第七次人口普查全国各省人口数(未包含港澳台",
+      "type":"fill",
+      "source":"第七次人口普查全国各省人口数(未包含港澳台",
+      "minzoom":0,
+      "maxzoom":22,
+      "layout":{},
+      "paint":{"fill-color":{"r":0.5098039215686274,"g":0.42745098039215684,"b":0.7294117647058823,"a":1},
+      "fill-opacity":0.9}
+    }
+    // 设置过了条件，点击的行政区域
+    const filter = '["all",["==","地区","青海"]]'
+    
+    // 
+    identifyWrapper.viewModel.addOverlayToMap(layer, filter)
+
+    // 确保修改图层的函数被执行
+    expect(spy).toHaveBeenCalledTimes(1);
+    
+    // 判断图层颜色是否被修改
+    expect(identifyWrapper.viewModel.layerStyle.stokeLine.paint['line-color']).toBe(color)  
+
     done();
   });
 
