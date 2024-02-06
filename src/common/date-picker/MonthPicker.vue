76baa5b574c7b1b557deb120206c8dbbb30859ea
chenxianhui <chenxianhui@supermap.com>
2024-02-06 17:13:03 +0800

    修改部分storybook样式 review by qiw

--- src/common/date-picker/MonthPicker.vue
+++ src/common/date-picker/MonthPicker.vue
@@ -4,7 +4,11 @@ import { MonthPickerProps } from 'ant-design-vue/es/date-picker/interface';
 import Base from './BaseMixin.vue';
 
 export const monthPickerTypes = {
-  ...MonthPickerProps()
+  ...MonthPickerProps(),
+  align: {
+    type: Object,
+    default: () => ({ points: ['tl', 'bl'], offset: [0, 4] })
+  }
 };
 
 export default {
--- src/common/date-picker/WeekPicker.vue
+++ src/common/date-picker/WeekPicker.vue
@@ -4,7 +4,11 @@ import { WeekPickerProps } from 'ant-design-vue/es/date-picker/interface';
 import Base from './BaseMixin.vue';
 
 export const weekPickerTypes = {
-  ...WeekPickerProps()
+  ...WeekPickerProps(),
+  align: {
+    type: Object,
+    default: () => ({ points: ['tl', 'bl'], offset: [0, 4] })
+  }
 };
 
 export default {
