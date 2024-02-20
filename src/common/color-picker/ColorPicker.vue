a3d858126e4557bef9428af34cfec63a62a268f8
songyumeng <songyumeng@supermap.com>
2024-02-20 17:02:23 +0800

    【fix】Group parts of the regex together to make the intended operator precedence explicit.

--- src/common/color-picker/ColorPicker.vue
+++ src/common/color-picker/ColorPicker.vue
@@ -63,7 +63,7 @@ export default class ColorPicker extends Vue {
         hex: color
       };
     } else if (/^rgba.+/.test(color)) {
-      const rgba = color.replace(/(?:^rgba?)\(|\s+|\)$/g, '').split(',');
+      const rgba = color.replace(/(?:^rgba?)(?:\(|\s+|\)$)/g, '').split(',');
       const hex = `#${((1 << 24) + (parseInt(rgba[0]) << 16) + (parseInt(rgba[1]) << 8) + parseInt(rgba[2]))
         .toString(16)
         .slice(1)}`;
