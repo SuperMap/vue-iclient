be36d870c342b8d2080de6761bad5d40ead54cf0
xiongjj <xiongjiaojiao@supermap.com>
2024-04-27 12:19:38 +0800

    【ut】fix ut

--- src/mapboxgl/drill-map/__tests__/DrillMap.spec.js
+++ src/mapboxgl/drill-map/__tests__/DrillMap.spec.js
@@ -10,7 +10,7 @@ import provinceInfo from 'vue-iclient/test/unit/mocks/data/DrillMap/provinceInfo
 import cityInfo from 'vue-iclient/test/unit/mocks/data/DrillMap/cityInfo.json';
 import countyInfo from 'vue-iclient/test/unit/mocks/data/DrillMap/countyInfo.json';
 import flushPromises from 'flush-promises';
-import mapWrapperLoaded from 'vue-iclient/test/unit/mapWrapperLoaded.js';
+import cloneDeep from 'lodash.clonedeep';
 
 const localVue = createLocalVue();
 localVue.use(Button);
@@ -41,22 +41,39 @@ const getDrillingMapData = function (mapIds = [244114284, 244114284, 244114284])
 };
 
 describe('DrillMap.vue', () => {
-  let wrapper;
-  const spy = jest.spyOn(mapboxgl, 'Map');
+  let wrapper, mapWrapper, spy;
 
   beforeAll(() => {
-    config.mapLoad = false;
+    // config.mapLoad = false;
   });
 
   beforeEach(() => {
-    wrapper = null;
+    spy = jest.spyOn(mapboxgl, 'Map');
+    const fetchResource = {
+      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
+      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': cloneDeep(mapInfo),
+      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        provinceInfo,
+      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        cityInfo,
+      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        countyInfo
+    };
+    mockFetch(fetchResource);
   });
 
   afterEach(() => {
-    jest.restoreAllMocks();
     if (wrapper) {
       wrapper.destroy();
+      wrapper = null;
     }
+    if (mapWrapper) {
+      mapWrapper.destroy();
+      mapWrapper = null;
+    }
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
   afterAll(() => {
@@ -64,14 +81,6 @@ describe('DrillMap.vue', () => {
   });
 
   it('initial_same_map', async done => {
-    const fetchResource = {
-      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
-    };
-    mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       propsData: {
@@ -80,22 +89,18 @@ describe('DrillMap.vue', () => {
       }
     });
     await flushPromises();
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(wrapper.vm.mapProps.target).toBe('map1');
-    expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
-    done();
+    await wrapper.vm.$nextTick();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
+    function callback() {
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(wrapper.vm.mapProps.target).toBe('map1');
+      expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
+      done();
+    }
   });
 
   it('initial_Control', async done => {
-    const fetchResource = {
-      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
-    };
-    mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       stubs: ['SmPan', 'SmScale', 'SmZoom'],
@@ -113,73 +118,79 @@ describe('DrillMap.vue', () => {
           show: true,
           position: 'top-left',
           showZoomSlider: false
-        }
+        },
+        target: 'map2'
       }
     });
     await flushPromises();
     await wrapper.vm.$nextTick();
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
-    expect(spy).toHaveBeenCalledTimes(1);
-    const map = wrapper.find('#map');
-    expect(map.element.id).toEqual('map');
-    expect(wrapper.vm.panControl.show).toBe(true);
-    expect(wrapper.vm.scaleControl.position).toBe('bottom-right');
-    expect(wrapper.vm.zoomControl.showZoomSlider).toBe(false);
-    expect(wrapper.find('.sm-component-pan').exists()).toBe(false);
-    expect(map.element.outerHTML).toContain('pan');
-    expect(map.element.outerHTML).toContain('zoom');
-    expect(map.element.outerHTML).toContain('scale');
-    done();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
+    function callback() {
+      expect(spy).toHaveBeenCalledTimes(1);
+      const map = wrapper.find('#map2');
+      expect(map.element.id).toEqual('map2');
+      expect(wrapper.vm.panControl.show).toBe(true);
+      expect(wrapper.vm.scaleControl.position).toBe('bottom-right');
+      expect(wrapper.vm.zoomControl.showZoomSlider).toBe(false);
+      expect(wrapper.find('.sm-component-pan').exists()).toBe(false);
+      expect(map.element.outerHTML).toContain('pan');
+      expect(map.element.outerHTML).toContain('zoom');
+      expect(map.element.outerHTML).toContain('scale');
+      done();
+    }
   });
 
   it('initial_diffrent_map', async done => {
     const fetchResource = {
       'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/1653065660/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/maps/891303728/map.json': mapInfo, //  其他
-      'https://fakeiportal.supermap.io/iportal/web/maps/366831804/map.json': mapInfo, //  其他
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
+      'https://fakeiportal.supermap.io/iportal/web/maps/1653065660/map.json': cloneDeep(mapInfo),
+      'https://fakeiportal.supermap.io/iportal/web/maps/891303728/map.json': cloneDeep(mapInfo), //  其他
+      'https://fakeiportal.supermap.io/iportal/web/maps/366831804/map.json': cloneDeep(mapInfo), //  其他
+      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        provinceInfo,
+      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        cityInfo,
+      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined':
+        countyInfo
     };
     mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       propsData: {
-        data: getDrillingMapData([1653065660, 891303728, 366831804])
+        data: getDrillingMapData([1653065660, 891303728, 366831804]),
+        target: 'map3'
       }
     });
     await flushPromises();
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
-    const layers = wrapper.vm.mapProps.mapId.layers;
-    const layerIDs = layers.map(item => item.name);
-    expect(layers.length).toEqual(3);
-    expect(layerIDs).toContain('云贵川');
-    expect(layerIDs).toContain('云贵川市');
-    expect(layerIDs).toContain('云贵川区县');
-    done();
+    await wrapper.vm.$nextTick();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
+    function callback() {
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
+      const layers = wrapper.vm.mapProps.mapId.layers;
+      const layerIDs = layers.map(item => item.name);
+      expect(layers.length).toEqual(3);
+      expect(layerIDs).toContain('云贵川');
+      expect(layerIDs).toContain('云贵川市');
+      expect(layerIDs).toContain('云贵川区县');
+      done();
+    }
   });
 
   it('drill_next_map', async done => {
-    const fetchResource = {
-      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
-    };
-    mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       propsData: {
-        data: getDrillingMapData()
+        data: getDrillingMapData(),
+        target: 'map4'
       }
     });
     await flushPromises();
-    wrapper.find(SmWebMap).vm.$on('load', callback);
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
+    await wrapper.vm.$nextTick();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
     function callback(e) {
       expect(spy).toHaveBeenCalledTimes(1);
       const map = e.map;
@@ -221,7 +232,7 @@ describe('DrillMap.vue', () => {
       ]);
       const spy2 = jest.spyOn(wrapper.vm.viewModel, 'getBboxByFeature');
       spy2.mockReturnValue([]);
-      expect(wrapper.vm.mapProps.target).toBe('map');
+      expect(wrapper.vm.mapProps.target).toBe('map4');
       expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
       wrapper.vm.viewModel.drillNextMap({
         target: map,
@@ -240,18 +251,11 @@ describe('DrillMap.vue', () => {
   });
 
   it('go_back', async done => {
-    const fetchResource = {
-      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
-    };
-    mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       propsData: {
-        data: getDrillingMapData()
+        data: getDrillingMapData(),
+        target: 'map5'
       },
       scopedSlots: {
         goBack: `<a-button id="go-back" @click="props.goBack">返回</a-button>`
@@ -259,7 +263,6 @@ describe('DrillMap.vue', () => {
     });
     await flushPromises();
     await wrapper.vm.$nextTick();
-    wrapper.find(SmWebMap).vm.$on('load', callback);
     function callback(e) {
       expect(spy).toHaveBeenCalledTimes(1);
       const map = e.map;
@@ -301,7 +304,7 @@ describe('DrillMap.vue', () => {
       ]);
       const spy2 = jest.spyOn(wrapper.vm.viewModel, 'getBboxByFeature');
       spy2.mockReturnValue([]);
-      expect(wrapper.vm.mapProps.target).toBe('map');
+      expect(wrapper.vm.mapProps.target).toBe('map5');
       expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
       wrapper.vm.viewModel.drillNextMap({
         target: map,
@@ -323,29 +326,22 @@ describe('DrillMap.vue', () => {
       expect(map.getLayer(layerId).layout.visibility).toBe('visible');
       done();
     }
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
-    await flushPromises();
-    await wrapper.vm.$nextTick();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
   });
 
   it('click_blank', async done => {
-    const fetchResource = {
-      'https://fakeiportal.supermap.io/iportal/web/config/portal.json': iportal_serviceProxy,
-      'https://fakeiportal.supermap.io/iportal/web/maps/244114284/map.json': mapInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/719613442/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': provinceInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1687422166/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': cityInfo,
-      'https://fakeiportal.supermap.io/iportal/web/datas/1960447494/content.json?pageSize=9999999&currentPage=1&parentResType=MAP&parentResId=undefined': countyInfo
-    };
-    mockFetch(fetchResource);
     wrapper = mount(SmDrillMap, {
       localVue,
       propsData: {
-        data: getDrillingMapData()
+        data: getDrillingMapData(),
+        target: 'map6'
       }
     });
     await flushPromises();
-    wrapper.find(SmWebMap).vm.$on('load', callback);
-    await mapWrapperLoaded(wrapper.find(SmWebMap));
+    await wrapper.vm.$nextTick();
+    mapWrapper = wrapper.find(SmWebMap);
+    mapWrapper.vm.$on('load', callback);
     function callback(e) {
       expect(spy).toHaveBeenCalledTimes(1);
       const map = e.map;
@@ -387,7 +383,7 @@ describe('DrillMap.vue', () => {
       ]);
       const spy2 = jest.spyOn(wrapper.vm.viewModel, 'getBboxByFeature');
       spy2.mockReturnValue([]);
-      expect(wrapper.vm.mapProps.target).toBe('map');
+      expect(wrapper.vm.mapProps.target).toBe('map6');
       expect(wrapper.vm.mapProps.serverUrl).toEqual('https://fakeiportal.supermap.io/iportal');
       wrapper.vm.viewModel.drillNextMap({
         target: map,
@@ -417,3 +413,4 @@ describe('DrillMap.vue', () => {
     }
   });
 });
+
