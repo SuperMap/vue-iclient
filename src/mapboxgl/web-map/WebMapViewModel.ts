ccdb87dc4d8ce2f2f049ab078b81665130956689
chenxianhui <chenxianhui@supermap.com>
2024-06-21 08:58:03 +0800

    fix DV地图开启自动更新，MD添加后更新无效 review by luox

--- src/mapboxgl/web-map/WebMapViewModel.ts
+++ src/mapboxgl/web-map/WebMapViewModel.ts
@@ -1471,6 +1471,9 @@ export default class WebMapViewModel extends WebMapBase {
     };
     if (!this.map.getSource(layerID)) {
       this.map.addSource(layerID, source);
+    } else {
+      // @ts-ignore
+      this.map.getSource(layerID).setData(source.data);
     }
     const iconID = `imageIcon-${layerID}`;
     if (style.type === 'IMAGE_POINT') {
