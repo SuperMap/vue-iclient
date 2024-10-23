2886b1eb09a0f7559309e1fb7fff3a2909543f2b
qiwei <qiwei@supermap.com>
2024-10-23 17:13:03 +0800

    图片组件新增预览效果 review by luox

--- src/common/image/Image.vue
+++ src/common/image/Image.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="sm-component-image" :style="[getBackgroundStyle, getTextColorStyle]">
     <a
-      :class="['sm-component-image__link', realHref ? '': 'sm-component-image__noLink']"
+      :class="['sm-component-image__link']"
       :href="realHref"
+      @click="handleLinkClick"
       :target="target"
     >
-      <div v-if="src" class="sm-component-image__content" :style="[repeatStyle,imgUrl]"></div>
+      <div v-if="src" @click="startPreview" class="sm-component-image__content" :style="[repeatStyle, imgUrl]"></div>
       <i v-else class="sm-components-icon-tupian sm-component-image__defaultImg"></i>
     </a>
+    <sm-modal
+      v-model="previewVisible"
+      :zIndex="10000"
+      centered
+      :dialogClass="dialogClass"
+      :dialogStyle="dialogStyle"
+      :bodyStyle="bodyStyle"
+      :footer="null"
+    >
+      <img :src="src" @click="endPreview" :style="{'object-fit': 'contain', width: '100%', height: '100%'}"/>
+    </sm-modal>
   </div>
 </template>
 
 <script>
 import Theme from 'vue-iclient/src/common/_mixin/Theme';
 import { parseUrl } from 'vue-iclient/src/common/_utils/util';
+import SmModal from 'vue-iclient/src/common/modal/Modal.vue';
 
 export default {
   name: 'SmImage',
+  components: { SmModal },
   mixins: [Theme],
   props: {
     src: {
@@ -30,6 +44,10 @@ export default {
       type: String,
       default: ''
     },
+    preview: {
+      type: Boolean,
+      default: true
+    },
     target: {
       type: String,
       default: '_self'
@@ -37,6 +55,17 @@ export default {
   },
   data() {
     return {
+      dialogStyle: {
+        backgroundColor: 'transparent',
+        height: '100%'
+      },
+      dialogClass: 'sm-component-image__preview',
+      bodyStyle: {
+        padding: 0,
+        width: '100%',
+        height: '100%'
+      },
+      previewVisible: false,
       repeatOption: {
         center: {
           backgroundRepeat: 'no-repeat',
@@ -77,6 +106,22 @@ export default {
         return `http://${href}`;
       }
       return href;
+    }
+  },
+  methods: {
+    handleLinkClick(e) {
+      if (!this.href) {
+        e.preventDefault();
+      }
+    },
+    startPreview() {
+      if (!this.preview) {
+        return;
+      }
+      this.previewVisible = true;
+    },
+    endPreview() {
+      this.previewVisible = false;
     }
   }
 };
