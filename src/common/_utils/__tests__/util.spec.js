1d19b21633e07ce3a195b3644972aff5f5b2f8d5
luoxiao <luoxiao@supermap.com>
2024-11-08 17:22:22 +0800

    [fix]UT

--- src/common/_utils/__tests__/util.spec.js
+++ src/common/_utils/__tests__/util.spec.js
@@ -1,7 +1,14 @@
-import { getDateTime } from '../util';
+import { getDateTime, escapeHTML } from '../util';
 
 describe('util test', () => {
   it('getDateTime ', () => {
     expect(getDateTime('date+second+week')).not.toBeNull();
+  });
+  it('escapeHTML ', () => {
+    const data1 = `helfo`
+    expect(escapeHTML`helfo`).toBe('helfo');
+    const data = `<br>//\helfo`
+    expect(escapeHTML`${data}`).toBe('&lt;br&gt;&#x2F;&#x2F;helfo');
+    expect(escapeHTML``).toBe('');
   });
 });
