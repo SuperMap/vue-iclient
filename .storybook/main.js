81b99d5ae17e75908b3b50da9bea69adf0e37f71
luoxiao <luoxiao@supermap.com>
2024-12-09 15:07:36 +0800

    [fix]storybook打包优化

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,15 @@ const path = require('path');
 function resolve(dir) {
   return path.join(__dirname, '..', dir);
 }
+const os = require('os');
+const threads = os.cpus().length - 1;
+const TerserPlugin = require('terser-webpack-plugin');
+// const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+// const smp = new SpeedMeasurePlugin();
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js)'],
   // 没有用到 @storybook/addon-postcss,隐藏关于postcss的警告
   features: {
     postcss: false
@@ -23,18 +30,40 @@ module.exports = {
     '@storybook/addon-toolbars'
   ],
   webpackFinal: async (config, { configType }) => {
-    config.module.noParse = /(mapbox-gl-enhance)\.js$/,
+    config.module.noParse = /(mapbox-gl-enhance)\.js$/;
     config.resolve.alias['vue-iclient'] = resolve('./');
     config.resolve.alias['@supermapgis/vue-iclient-mapboxgl/static'] = resolve('./static');
     config.resolve.alias['@supermapgis/vue-iclient-mapboxgl/lib'] = resolve('./lib/mapboxgl');
     config.resolve.alias['@supermapgis/vue-iclient-mapboxgl'] = resolve('./lib/mapboxgl');
     config.optimization = {
       splitChunks: {
+        cacheGroups: {
+          vendors: {
+            test: /node_modules/,
+            name: 'vendors',
+            minChunks: 1,
+            chunks: 'initial',
+            minSize: 0,
+            priority: 1
+          },
+          commons: {
+            name: 'commons',
+            minChunks: 2,
+            chunks: 'initial',
+            minSize: 0
+          }
+        },
         chunks: 'all',
         minSize: 30 * 1024, // 30KB
         maxSize: 1024 * 1024 // 1MB
-      }
+      },
+      minimizer: [
+        new TerserPlugin({
+          parallel: threads // 开启多进程
+        })
+      ]
     };
+    config.resolve.extensions = ['.js', '.ts', '.json'];
     config.externals = [
       function (context, request, callback) {
         if (request === '@supermapgis/vue-iclient-mapboxgl') {
@@ -47,6 +76,8 @@ module.exports = {
       test: /useLib\.js$/,
       loader: 'babel-loader',
       options: {
+        cacheDirectory: true, // 开启babel编译缓存
+        cacheCompression: false, // 缓存文件不要压缩
         presets: ['@babel/preset-env'],
         plugins: [
           ['@babel/plugin-proposal-private-methods', { loose: true }],
