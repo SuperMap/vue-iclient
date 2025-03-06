cda9cf7f3cde8a473a51e30b3a97288a3f54b903
luoxiao <luoxiao@supermap.com>
2025-03-06 09:09:02 +0800

    [fix]bug

--- vue3/packages/mapboxgl/vue-iclient-mapboxgl/component.ts
+++ vue3/packages/mapboxgl/vue-iclient-mapboxgl/component.ts
@@ -2,7 +2,7 @@ import { SmConfigProvider } from '@supermapgis/common/components/config-provider
 import { SmAttributes } from '@supermapgis/mapboxgl/components/attributes'
 import { SmWebMap } from '@supermapgis/mapboxgl/components/web-map'
 import { SmGraphMap } from '@supermapgis/mapboxgl/components/graph-map'
-import { SmCompare } from '@supermapgis/mapboxgl/components/COMPARE'
+import { SmCompare } from '@supermapgis/mapboxgl/components/compare'
 
 import type { Plugin } from 'vue'
 
