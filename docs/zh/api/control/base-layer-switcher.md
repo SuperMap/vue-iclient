dc1ffdc5f4909f89982bfed69411af459ac861d4
xiongjj <xiongjiaojiao@supermap.com>
2025-08-16 21:24:42 +0800

    【fix】底图切换组件监听地图后加载优化;底图支持传title和缩略图; review by chexh

--- docs/zh/api/control/base-layer-switcher.md
+++ docs/zh/api/control/base-layer-switcher.md
@@ -8,11 +8,11 @@
 
 ### Attributes
 
-| 参数            | 说明               | 类型                                                   | 可选值  | 默认值 |
-|:----------------|:-----------------|:-------------------------------------------------------|:--------|:-------|
-| layers          | 底图列表           | Array<[LayerItem](/zh/api/control/query.md#layeritem)> | -       | -      |
-| defaultLayer    | 默认底图的唯一标识 | string                                                 | string  | -      |
-| showOriginLayer | 是否显示原始底图   | boolean                                                | boolean | true   |
+| 参数          | 说明               | 类型                            | 可选值  | 默认值 |
+|:--------------|:-----------------|:--------------------------------|:--------|:-------|
+| layers        | 底图列表           | Array<[LayerItem](#layeritem)>  | -       | -      |
+| defaultLayer  | 默认底图的唯一标识 | string                          | string  | -      |
+| baseLayerInfo | 原始底图信息       | [BaseLayerInfo](#baselayerinfo) | boolean | true   |
 
 > 支持[主题混入参数](/zh/api/mixin/mixin.md#theme)和 [Control 混入参数](/zh/api/mixin/mixin.md#control)
 
@@ -25,3 +25,11 @@
 | layers    | MapboxGL 图层   | Array<[Layers](https://docs.mapbox.com/style-spec/reference/layers/)>   | 是       |
 | sources   | MapboxGL 数据源 | Array<[Sources](https://docs.mapbox.com/style-spec/reference/sources/)> | 是       |
 | thumbnail | 底图缩略图      | string                                                                  | 否       |
+
+### BaseLayerInfo
+
+| 参数      | 说明       | 类型    | 是否必填 |
+|:----------|:---------|:--------|:--------|
+| show      | 显示原底图 | boolean | 是       |
+| title     | 底图名称   | string  | 是       |
+| thumbnail | 底图缩略图 | string  | 否       |
--- src/mapboxgl/base-layer-switcher/BaseLayerSwitcherViewModel.ts
+++ src/mapboxgl/base-layer-switcher/BaseLayerSwitcherViewModel.ts
@@ -1,3 +1,4 @@
+import cloneDeep from 'lodash.clonedeep';
 import mapboxgl from 'vue-iclient/static/libs/mapboxgl/mapbox-gl-enhance';
 
 interface LayerEnhance extends mapboxglTypes.Layer {
@@ -47,31 +48,63 @@ export default class BaseLayerSwitcherViewModel extends mapboxgl.Evented {
   removed() {
     this._resetBaseLayer();
     this.map = null;
+    this.webmap = null;
+    this.baseLayer = null;
   }
 
   changeBaseLayer(layer: LayerItem) {
-    if (this.webmap) {
-      this.webmap.changeBaseLayer(layer);
+    if (this.webmap && layer) {
+      const nextLayer: LayerItem = {
+        ...layer,
+        layers: layer.layers.map(item => {
+          return {
+            ...item,
+            metadata: {
+              ...item.metadata,
+              title: item.metadata?.title || layer.title
+            }
+          };
+        })
+      };
+      this.webmap.changeBaseLayer(nextLayer);
       this._currentBaseLayerInfo = layer;
     }
   }
 
+  setBaseTitle(title: string, changeBaseLayer: boolean) {
+    if (this.baseLayer) {
+      const nextBaseLayer = cloneDeep(this.baseLayer);
+      nextBaseLayer.title = title || this.baseLayer.title;
+      if (nextBaseLayer.layers.length === 1) {
+        nextBaseLayer.layers[0].metadata.title = title || this.baseLayer.layers[0].metadata.title;
+      }
+      changeBaseLayer && this.changeBaseLayer(nextBaseLayer);
+      this.fire('baselayerchanged', { baseLayer: nextBaseLayer });
+    }
+  }
+
   private _onLayersUpdated() {
-    if (!this.baseLayer) {
+    if (!this.baseLayer && this.map) {
       this._initOriginBaseLayer();
     }
   }
 
   private _initOriginBaseLayer() {
+    const mapStyle = this.map.getStyle();
+    if (!mapStyle) {
+      return;
+    }
+    const { layers: layerList, sources: sourcesMap } = mapStyle;
+    if (layerList.length === 0) {
+      return;
+    }
     const appreciableLayers = this.webmap.getAppreciableLayers();
     const layerCatalog = this.webmap.getLayerList().slice();
     const baseLayerCatalog = layerCatalog.pop();
-    const { layers: layerList, sources: sourcesMap } = this.map.getStyle();
     const baseLayers = this._getBaseLayerRenderLayers(baseLayerCatalog, appreciableLayers, layerList);
-    const defaultBaseLayer = appreciableLayers[0];
     this.baseLayer = {
-      id: `__default__${defaultBaseLayer.id}`,
-      title: defaultBaseLayer.title,
+      id: `__default__${baseLayerCatalog.id}`,
+      title: baseLayerCatalog.title,
       layers: baseLayers,
       sources: baseLayers.reduce((sources, layer) => {
         const sourceId = layer.source as unknown as string;
@@ -83,10 +116,14 @@ export default class BaseLayerSwitcherViewModel extends mapboxgl.Evented {
       }, {})
     };
     this._currentBaseLayerInfo = this.baseLayer;
-    this.fire('baselayerchanged', this.baseLayer);
+    this.fire('baselayerchanged', { baseLayer: this.baseLayer });
   }
 
-  private _getBaseLayerRenderLayers(layerCatalog: any[], appreciableLayers: any[], layersOnMap: LayerEnhance[]): LayerEnhance[] {
+  private _getBaseLayerRenderLayers(
+    layerCatalog: Record<string, any>,
+    appreciableLayers: any[],
+    layersOnMap: LayerEnhance[]
+  ): LayerEnhance[] {
     const uniqueSet = new Set();
     const collectIds = (node: Record<string, any>) => {
       if (node.children) {
@@ -95,15 +132,26 @@ export default class BaseLayerSwitcherViewModel extends mapboxgl.Evented {
       node.renderLayers && node.renderLayers.forEach((part: string) => uniqueSet.add(part));
     };
     collectIds(layerCatalog);
+
     const layers = appreciableLayers.filter(layer => layer.renderLayers.some((id: string) => uniqueSet.has(id)));
     return layers.reduce((layerList, layer) => {
-      const subLayers = layer.renderLayers.map((id: string) => Object.assign({}, layersOnMap.find(layer => layer.id === id)));
+      const subLayers = layer.renderLayers.map((id: string) => {
+        const layerInfo = Object.assign(
+          {},
+          layersOnMap.find(layer => layer.id === id)
+        );
+        layerInfo.metadata = {
+          ...layerInfo.metadata,
+          title: layerInfo.metadata?.title || layer.title
+        };
+        return layerInfo;
+      });
       return layerList.concat(subLayers);
     }, []);
   }
 
   private _resetBaseLayer() {
-    if (this.baseLayer && this._currentBaseLayerInfo && this._currentBaseLayerInfo.id !== this.baseLayer.id) {
+    if (this.baseLayer && this._currentBaseLayerInfo) {
       this.changeBaseLayer(this.baseLayer);
     }
   }
