35380011ef9c6db88d02a8ec7334ea48de81f395
Huzhipeng <huzhipeng@supermap.com>
2026-01-30 09:49:34 +0800

    [update] mapboxFilterToCqlFilter转换逻辑中结构化字段名加入双引号 (review by xjj)

--- src/mapboxgl/web-map/WebMapViewModel.ts
+++ src/mapboxgl/web-map/WebMapViewModel.ts
@@ -344,7 +344,7 @@ export default class WebMapViewModel extends Events {
           id: dataId,
           dataType: 'STRUCTUREDDATA',
           url: Util.urlPathAppend(this.serverUrl, `web/datas/${dataId}`),
-          attributeFilter: mapboxFilterToQueryFilter(item.filter)
+          attributeFilter: mapboxFilterToQueryFilter(item.filter, item.dataSource.type)
         };
         break;
       }
