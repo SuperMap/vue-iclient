1ce1940332c43c1fb788221f764c2484f6d891c2
luoxiao <luoxiao@supermap.com>
2026-07-09 15:52:13 +0800

    [fix]attributepopup resizeCallback 循环问题 AI-GEN: 100% Cursor

--- src/mapboxgl/web-map/control/attribute-popup/mixins/resize-mixin.js
+++ src/mapboxgl/web-map/control/attribute-popup/mixins/resize-mixin.js
@@ -1,4 +1,5 @@
 import { addListener, removeListener } from 'resize-detector';
+import debounce from 'lodash.debounce';
 
 /**
  * Resize Mixin - 响应式监听
@@ -11,7 +12,12 @@ export default {
   },
   methods: {
     addResizeListener(el, callback) {
-      this.resizeCallback = el => callback(el);
+      if (this.resizeCallback) {
+        this.resizeCallback.cancel();
+      }
+      this.resizeCallback = debounce(el => {
+        requestAnimationFrame(() => callback(el));
+      }, 100, { leading: false, trailing: true });
       if (el) {
         addListener(el, this.resizeCallback);
       }
@@ -19,9 +25,8 @@ export default {
 
     removeResizeListener(el) {
       if (el && this.resizeCallback) {
-        // resize-detector 不直接提供 removeListener
-        // 如果需要，可以在组件销毁时清理监听器
         removeListener(el, this.resizeCallback);
+        this.resizeCallback.cancel();
         this.resizeCallback = null;
       }
     }
